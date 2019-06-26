cc099f1d6a48707aa3e2a1a4723552de7153491e
Ladun <ladun9128@gmail.com>
2019-06-26 15:16:52 +0900

    Delete All'

--- src/com/ladun/engine/AbstractGame.java
+++
@@ -1,8 +0,0 @@
-package com.ladun.engine;
-
-public abstract class AbstractGame {
-
-	public abstract boolean init(GameContainer gc);
-	public abstract void update(GameContainer gc,float dt);
-	public abstract void render(GameContainer gc, Renderer r);
-}
